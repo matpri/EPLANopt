3f8bee4c0b5587c62b57dd8195be3376ef7370ee
Matteo Giacomo prina <matteogiacomo.prina@eurac.edu>
2024-11-08 14:44:28 +0100

    Update TestGA.py

--- TestGA.py
+++ TestGA.py
@@ -8,7 +8,6 @@ Genetic Algorith implemented with deap algorith
 version 1.1 to install from master branch of github repository
 pip install -U git+http://github.com/DEAP/deap
 """
-
 from GA import GA
 from libeplan import Node
 from termcolor import colored
@@ -19,16 +18,17 @@ global ENERGYPLAN, OUT_FOLDER, FUNCTION_2_EVAL
 global CONSTRAIN
 
 #------------------------------------INPUT DATA--------------------------------
-data = {"EnergyPLAN folder": r"C:\Users\MPrina\Desktop\ZIPEnergyPLAN121",
-        "Input file": r"C:\Users\MPrina\Desktop\SDEWES 2020\EPLANopt Island\EPLANopt\input_folder\Denmark2030Reference_ANSI.txt",
-        "Output folder": r"C:\Users\MPrina\Desktop\SDEWES 2020\EPLANopt Island\EPLANopt\input_folder",
+
+data = {"EnergyPLAN folder": r"C:\Users\MPrina\Desktop\ZipEnergyPLAN161",
+        "Input file": r"C:\Users\MPrina\Desktop\INEST PNRR\EPLANopt-main (2)\EPLANopt-main\INPUT\Denmark2030Reference_ANSI.txt",
+        "Output folder": r"C:\Users\MPrina\Desktop\INEST PNRR\EPLANopt-main (2)\EPLANopt-main\OUTPUT",
         "Number of process": 1,
         "Genetic algorithm": {"Size of population": 4,
                                "Number of generations": 1},
 
         "Variables": [{"EnergyPLAN Name": "input_RES1_capacity", "Range": [17, 100],"Moltiplication factor": 10}, #[10, 50]
-                      {"EnergyPLAN Name": 'input_cap_pp_el', "Range": [0, 0],"Moltiplication factor": 10}, #[0, 1]
-                      {"EnergyPLAN Name": 'input_storage_pump_cap', "Range": [0, 0],"Moltiplication factor": 10}] #[0, 75]
+                      {"EnergyPLAN Name": 'input_cap_pp_el', "Range": [0, 10],"Moltiplication factor": 10}, #[0, 1]
+                      {"EnergyPLAN Name": 'input_storage_pump_cap', "Range": [0, 100],"Moltiplication factor": 10}] #[0, 75]
         }
         
 #------------------------------------INITIALIZING------------------------------
@@ -63,7 +63,7 @@ def f(individual):
         new_data[key] = varEP[i]
 
     INPUTFILE = INPUTFILE.replace('.txt','new_node'+'.txt')
-    out_file = r'%s\out_new.txt' % (OUT_FOLDER)
+    out_file = r'%s\output.txt' % (OUT_FOLDER)
 
     new_node = Node(INPUTFILE, ENERGYPLAN, out_file, new_data)
 
@@ -90,13 +90,13 @@ def f(individual):
 
     CO2 = dic['CO2-emission (total)'] # CO2 [kt]
     
-    CEEP= dic['CO2-emission (total)']
+    #CEEP= dic['CO2-emission (total)']
     print('Costs:', colored(Costs, 'red'), '[M€]')
     print('CO2:', colored(CO2, 'green'), '[kt]')
-    print('CEEP:', colored(CEEP, 'blue'), '[kt]')
-    return CO2, Costs, CEEP
+    #print('CEEP:', colored(CEEP, 'blue'), '[kt]')
+    return CO2, Costs
     
-objectives = (-1.0, -1.0, -1.0,)
+objectives = (-1.0, -1.0)
 pop, ff, hist = GA(X, f, objectives, NPOP, NGEN)
 #if __name__ == "__main__":
 t0 = time.time()
@@ -109,16 +109,18 @@ hist_file = open('history_csv.csv', 'w')
 for i in range(len(VARIABLES)):
     hist_file.write(str(VARIABLES[i]) + ',')
 hist_file.write('CO2 emissions [kt]'+ ',')
-hist_file.write('Total annual costs [k€]'+ ',')
-hist_file.write('CEEP [TWh]'+ ',')
+hist_file.write('Total annual costs [keuro]'+ ',')
+# hist_file.write('CEEP [TWh]'+ ',')
 hist_file.write('\n')
 for a in range(len(hist['population'])):
     for b in range(len(hist['population'][0])):
         for c in range(len(hist['population'][0][0])):
             hist_file.write(str(hist['population'][a][b][c]*MOLT_FACTORS[c]) + ',')
-        for d in range(len(hist['fitness'][0][0])):
-            hist_file.write(str(hist['fitness'][a][b][d]) + ',')
+        # Write fitness data
+        fitness_data = [str(hist['fitness'][a][b][d]) for d in range(len(hist['fitness'][0][0]))]
+        hist_file.write(','.join(fitness_data))  # Join fitness data without trailing comma
         hist_file.write('\n')
+        
 hist_file.close()
 
 
