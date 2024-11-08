c08b15db57b549cac9fa79336bd5b6eb7fc0fd85
Matteo Giacomo prina <matteogiacomo.prina@eurac.edu>
2024-11-08 14:45:32 +0100

    Update Pareto.py

--- Pareto.py
+++ Pareto.py
@@ -31,18 +31,23 @@ def pareto_front(lst):
     return [lst[x] for x in range(nlst) if x not in idi]
 
 '''read the history_csv.csv file and calculate the pareto front of non-dominated solutions'''
-#df_hist = pd.read_csv('history_csv100.csv', encoding = "ISO-8859-1")
-df_hist = pd.read_excel('output.xlsx', encoding = "ISO-8859-1")
-df_hist = df_hist.loc[:, ~df_hist.columns.str.contains('^Unnamed')][1:]
-df_hist = df_hist.convert_objects(convert_numeric=True)
+df_hist = pd.read_csv('history_csv.csv', encoding = "ISO-8859-1")
+# df_hist = pd.read_excel('output.xlsx')
+#df_hist = df_hist.loc[:, ~df_hist.columns.str.contains('^Unnamed')][1:]
+# Remove columns with 'Unnamed' in their column names
+df_hist = df_hist.loc[:, ~df_hist.columns.str.contains('^Unnamed')]
+
+df_hist = df_hist.apply(pd.to_numeric, errors='coerce')
+#df_hist = df_hist.convert_objects(convert_numeric=True)
 #df_hist = pd.to_numeric(df_hist)#convert_objects(convert_numeric=True)
 #print(df_hist)
+print(df_hist)
 dic=df_hist.T.to_dict('list')
 #print(dic)
 list_individuals=[]
 for a in range(len(dic)):
 #    print(dic[a+1][-1])
-    row=[dic[a+1][-2], dic[a+1][-1]]
+    row=[dic[a][-2], dic[a][-1]]
     list_individuals.append(row)
 #    print(dic[a][-1])
 #print(list_individuals)
@@ -54,7 +59,8 @@ print(pareto)
 df_pareto=pd.DataFrame()
 for a in range(len(pareto)):
     df_el= df_hist.loc[(df_hist['CO2 emissions [kt]']== pareto[a][0]) & (df_hist['Total annual costs [keuro]']== pareto[a][1])]
-    df_pareto= df_pareto.append(df_el)
+    df_pareto = pd.concat([df_pareto, df_el])
+    # df_pareto= df_pareto.append(df_el)
 
 '''create a result xlsx file with hist and pareto'''
 #     Create a Pandas Excel writer using XlsxWriter as the engine.
@@ -64,35 +70,4 @@ df_hist.to_excel(writer, sheet_name='history')
 df_pareto.to_excel(writer, sheet_name='Pareto')
     
 #     Close the Pandas Excel writer and output the Excel file.
-writer.save()
-
-#'''plot the results'''
-#CO2_ind=51.16 #[Mt]
-#cost_ind=4714 #[M€]
-##cost_ind=35851 #[M€]
-##cost_ind=7951.73 #[M€]
-#
-#df_hist['CO2']=df_hist.loc[:,'CO2 [Mt]']+CO2_ind
-#df_hist['Costs']=df_hist.loc[:,'Costs [M€]']+cost_ind
-#df_pareto['CO2']=df_pareto.loc[:,'CO2 [Mt]']+CO2_ind
-#df_pareto['Costs']=df_pareto.loc[:,'Costs [M€]']+cost_ind
-#
-#xhist= df_hist['CO2']
-#yhist= df_hist['Costs']
-#xpar= df_pareto['CO2']
-#ypar= df_pareto['Costs']
-#
-#fig, ax = plt.subplots(figsize=(14,8))
-#
-#ax.scatter(xhist, yhist, marker='o', c='blue')
-#ax.scatter(xpar, ypar, marker='o', c= 'green')
-#ax.scatter(278.58+CO2_ind, 69524.27+cost_ind, marker='o', s=100, c= 'red')
-##ax.scatter(331.77, 62649.82+cost_ind, marker='o', s=90, c= 'orange')
-#
-#ax.set_xlabel('CO2 emissions [Mt]')
-#ax.set_ylabel('Total annual costs [M€]')
-##plt.legend()
-#plt.grid()
-#plt.show()
-#
-#pylab.savefig("Pareto.png", bbox_inches="tight", dpi=300)
+writer.close()
